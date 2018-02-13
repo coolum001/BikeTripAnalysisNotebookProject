ee45149db666b285e0ff51efac431b50bc562b5a
Don Cameron <donrcameron@gmail.com>
2018-02-13 18:18:01 +1000

    Add pytest suite

---
+++ develop/test_bikedataloader.py
@@ -0,0 +1,44 @@
+from bikeloader import bikeloader as bl
+import tempfile
+import pandas
+
+import os
+
+
+def test_load():
+    '''
+    pytest function to test is pandas dataframe is in the expected format
+    
+    we test the data column names, and the index type
+    '''
+    url = 'https://data.seattle.gov/api/views/65db-xm6k/rows.csv?accessType=DOWNLOAD'
+    csv_file = '../data/fremont.csv'
+
+    bikedf = bl.load_csv_datetime_data(url=url, file_path = csv_file)
+    
+    assert bikedf.columns[0]=='West' and bikedf.columns[1]=='East'
+    
+    assert pandas.core.indexes.datetimes.DatetimeIndex==type(bikedf.index)
+    
+#end test_load
+
+def test_save():
+    '''
+    pytest function to test that downloaded url is saved to an os file
+    '''
+    url = 'https://data.seattle.gov/api/views/65db-xm6k/rows.csv?accessType=DOWNLOAD'
+    csv_file = '../data/pytest01.csv'
+    
+    os.remove(csv_file) if os.path.exists(csv_file) else None
+    
+    bikedf = bl.load_csv_datetime_data(url=url, file_path = csv_file)
+    
+    assert os.path.exists(csv_file)
+    
+    os.remove(csv_file) if os.path.exists(csv_file) else None
+    
+#end test_save
+    
+    
+    
+
