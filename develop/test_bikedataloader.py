4b711a6b0ae940ccc1c3edee63842fa7c9d843c9
Don Cameron <donrcameron@gmail.com>
2018-02-13 20:25:50 +1000

    Add assertion test to pytest suite

--- develop/test_bikedataloader.py
+++ develop/test_bikedataloader.py
@@ -3,6 +3,7 @@ import tempfile
 import pandas
 
 import os
+import pytest
 
 
 def test_load():
@@ -38,6 +39,26 @@ def test_save():
     os.remove(csv_file) if os.path.exists(csv_file) else None
     
 #end test_save
+
+def test_asserts():
+    '''
+    pytest function to test if empty input parameters raise assert exceptions
+    '''
+    url = None
+    csv_file = '../data/pytest01.csv'
+    
+    with pytest.raises(AssertionError):
+        bikedf = bl.load_csv_datetime_data(url=url, file_path = csv_file)
+    #end with
+    
+    url = 'https://data.seattle.gov/api/views/65db-xm6k/rows.csv?accessType=DOWNLOAD'
+    csv_file = None
+    
+    with pytest.raises(AssertionError):
+        bikedf = bl.load_csv_datetime_data(url=url, file_path = csv_file)
+    #end with
+    
+#end test_asserts
     
     
     
